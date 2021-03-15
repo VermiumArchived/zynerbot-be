980ac67500fa795d0be16c5c8a4cc459e7834440
Vermium Sifell <vermium@zyner.org>
2021-03-15 13:59:14 +0100

    Update index.js

--- discord/commands/utility/ping/index.js
+++ discord/commands/utility/ping/index.js
@@ -1,1 +1,9 @@
-
+module.exports = {
+  name: "Ping",
+  description: "Checks latency",
+  category: "Utility",
+  permissions: [],
+  execute: () => {
+    console.log("Executed command ping");
+  },
+};
